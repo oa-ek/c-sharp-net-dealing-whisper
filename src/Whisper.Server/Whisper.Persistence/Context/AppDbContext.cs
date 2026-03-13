39bd27a18faed168028341436a80c46e3fe54380
Chicago <92821486+illiadavydiuk@users.noreply.github.com>
2026-03-14 00:08:31 +0200

    feat: add backend skeleton

---
+++ src/Whisper.Server/Whisper.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Whisper.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+    }
+}
