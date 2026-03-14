f2a0fdf21dd4a12bf30c8498e67f98d4835df03d
Chicago <92821486+illiadavydiuk@users.noreply.github.com>
2026-03-14 21:00:45 +0200

    set up react and modify folder structure

--- src/Whisper.Client/vite.config.ts
+++ src/Whisper.Client/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:7249',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
