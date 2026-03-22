df7e73cbd028c4bc4963a87878d2178e06157bc5
Chicago <92821486+illiadavydiuk@users.noreply.github.com>
2026-03-22 20:57:12 +0200

    add auth dtos + token service

---
+++ src/Whisper.Server/Whisper.Application/DTOs/AuthDTOs/ChangePasswordDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Whisper.Application.DTOs.AuthDTOs
+{
+    public class ChangePasswordDto
+    {
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
