94f6661115eed826b2a4e3f4bd20ab8c70bfcd8d
Emisu <bogdan.lutsiuk@gmail.com>
2026-03-22 20:30:09 +0200

    added basic repository models Added all repository interfaces. Implemented basic repositories for users database but not messages one as context for it has not yet been created.

---
+++ src/Whisper.Server/Whisper.Application/Interfaces/Repositories/IChatRepository.cs
@@ -0,0 +1,9 @@
+﻿using Whisper.Domain.Entities;
+
+namespace Whisper.Application.Interfaces.Repositories
+{
+    public interface IChatRepository : IRepository<Chat>
+    {
+
+    }
+}
---
+++ src/Whisper.Server/Whisper.Persistence/Repositories/UserDeviceRepository.cs
@@ -0,0 +1,12 @@
+﻿using Whisper.Application.Interfaces.Repositories;
+using Whisper.Domain.Entities;
+using Whisper.Persistence.Context;
+
+namespace Whisper.Persistence.Repositories
+{
+    public class UserDeviceRepository : Repository<UserDevice>, IUserDeviceRepository
+    {
+        public UserDeviceRepository(AppDbContext context) : base(context) { }
+
+    }
+}
