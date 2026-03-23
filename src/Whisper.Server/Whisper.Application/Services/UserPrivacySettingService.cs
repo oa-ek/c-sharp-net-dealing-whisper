58347a6ad12f46a252609622f234af942575911a
Emisu <bogdan.lutsiuk@gmail.com>
2026-03-23 21:32:07 +0200

    feat: add basic service templates to use with repositories All done, all working

---
+++ src/Whisper.Server/Whisper.Application/Services/UserPrivacySettingService.cs
@@ -0,0 +1,15 @@
+﻿using Whisper.Application.Interfaces.Repositories;
+using Whisper.Application.Interfaces.Services;
+
+namespace Whisper.Application.Services
+{
+    public class UserPrivacySettingService : IUserPrivacySettingService
+    {
+        private readonly IUserPrivacySettingRepository _userPrivacySettingRepository;
+        public UserPrivacySettingService(IUserPrivacySettingRepository userPrivacySettingRepository)
+        {
+            _userPrivacySettingRepository = userPrivacySettingRepository;
+        }
+
+    }
+}
---
+++ src/Whisper.Server/Whisper.Application/Services/UserService.cs
@@ -0,0 +1,15 @@
+﻿using Whisper.Application.Interfaces.Repositories;
+using Whisper.Application.Interfaces.Services;
+
+namespace Whisper.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+    }
+}
